ffedb694662c00e7356249b7b82bcf5fcd395558
Dafi <dafi.almadinah0511@gmail.com>
2025-07-02 12:02:09 +0700

    feat: OurServicesPage 90%

--- src/section/OurServicesPage.tsx
+++ src/section/OurServicesPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 export const OurServicesPage = () => {
   return (
@@ -11,13 +10,13 @@ export const OurServicesPage = () => {
         </p>
       </div>
 
-      <div className="flex flex-row">
-        <div className="flex flex-col bg-gradient-to-r from-white to-[#E0EDFF] p-10 max-w-[44ch] rounded-md shadow-2xl gap-5">
-          <h1 className="font-bold">
-            MVP Development
-          </h1>
-          <p className="text-gray-800">
-            Perfect for startups looking to validate their ideas with a functional prototype
+      <div className="flex flex-row gap-10">
+        <div className="flex flex-col bg-gradient-to-r from-white to-[#E0EDFF] p-10 max-w-[44ch] rounded-2xl shadow-2xl gap-5">
+          <img src="/assets/roket.png" alt="roket" className="size-10" />
+          <h1 className="font-normal text-2xl">MVP Development</h1>
+          <p className="text-gray-700">
+            Perfect for startups looking to validate their ideas with a
+            functional prototype
           </p>
           <ol>
             <li>👽Rapid Prototyping</li>
@@ -25,15 +24,31 @@ export const OurServicesPage = () => {
             <li>👽Scalable Architecture</li>
           </ol>
         </div>
-        <div>
-            <h1>
-                Custom Software
-            </h1>
+        <div className="flex flex-col bg-gradient-to-r from-white to-[#EEDDFF] p-10 max-w-[44ch] rounded-2xl shadow-2xl gap-5">
+          <img src="/assets/tag.png" alt="roket" className="size-10" />
+          <h1 className="font-normal text-2xl">Custom Software</h1>
+          <p className="text-gray-700">
+            Tailored solutions for businesses with specific requirements and
+            workflows.
+          </p>
+          <ol>
+            <li>👽Bespoke Development</li>
+            <li>👽Integration Services</li>
+            <li>👽Legacy Modernization</li>
+          </ol>
         </div>
-        <div>
-            <h1>
-
-            </h1>
+        <div className="flex flex-col bg-gradient-to-r from-white to-[#E0EDFF] p-10 max-w-[44ch] rounded-2xl shadow-2xl gap-5">
+          <img src="/assets/topi.png" alt="roket" className="size-10" />
+          <h1 className="font-normal text-2xl">Student Consultation</h1>
+          <p className="text-gray-700">
+            Technical guidance and mentorship for academic projects and thesis
+            work
+          </p>
+          <ol>
+            <li>👽Project Mentorship</li>
+            <li>👽Technical Guidance</li>
+            <li>👽Code Review</li>
+          </ol>
         </div>
       </div>
     </section>
